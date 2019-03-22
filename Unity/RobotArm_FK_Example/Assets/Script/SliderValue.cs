144648e33b67ac2caadcc5d3a5e4e68f1f2a1d21
nwilliterate <nwilliterate@gmail.com>
2019-03-22 15:08:19 +0900

    역기구학 추가(모델의 맞는 범위 설정 필요)

--- Unity/RobotArm_FK_Example/Assets/Script/SliderValue.cs
+++ Unity/RobotArm_FK_Example/Assets/Script/SliderValue.cs
@@ -10,19 +10,40 @@ public class SliderValue : MonoBehaviour {
     private GameObject handleSlideArea;
     private GameObject handle;
 
-	// Use this for initialization
-	void Start () {
-        handleSlideArea = transform.Find("Handle Slide Area").gameObject;
-        handle = handleSlideArea.transform.Find("Handle").gameObject;
-        textValue = handle.transform.Find("Value").GetComponent<Text>();
+    private Text titleText;
+    private Slider[] ArtSlider = new Slider[6];
+    private Text[] ArtSliderTitle = new Text[6];
+    private Text[] ArtSliderMaxValue = new Text[6];
+    private Text[] ArtSliderMinValue = new Text[6];
 
+    // Use this for initialization
+    void Start () {
         slider = transform.GetComponent<Slider>();
-        
-        UpdateValue();
+
+        if (transform.gameObject.name.Equals("Slider"))
+        {
+            titleText = GameObject.Find("TitleText").GetComponent<Text>();
+            for (int i = 0; i < ArtSlider.Length; i++)
+                ArtSlider[i] = GameObject.Find("Axis" + (i + 1) + "Slider").transform.GetComponent<Slider>();
+            for (int i = 0; i < ArtSliderTitle.Length; i++)
+            {
+                ArtSliderTitle[i] = ArtSlider[i].transform.Find("Title").gameObject.GetComponent<Text>();
+                ArtSliderMaxValue[i] = ArtSlider[i].transform.Find("MaxValue").gameObject.GetComponent<Text>();
+                ArtSliderMinValue[i] = ArtSlider[i].transform.Find("MinValue").gameObject.GetComponent<Text>();
+            }
+        }
+        else
+        {
+            handleSlideArea = transform.Find("Handle Slide Area").gameObject;
+            handle = handleSlideArea.transform.Find("Handle").gameObject;
+            textValue = handle.transform.Find("Value").GetComponent<Text>();
+
+            UpdateValue();
+        }
     }
     public void UpdateValue ()
     {
-        slider.value = (int)slider.value;
+        //slider.value = (int)slider.value;
         textValue.text = slider.value.ToString();
     }
 	
@@ -30,4 +51,71 @@ public class SliderValue : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void OnChangedKinematics()
+    {
+        switch (slider.value)
+        {
+            case 0:
+                ArtSlider[0].value = 0;
+                ArtSlider[1].value = -45;
+                ArtSlider[2].value = 90;
+                ArtSlider[3].value = 0;
+                ArtSlider[4].value = 45;
+                ArtSlider[5].value = 0;
+
+
+                titleText.text = "Forward Kinematics";
+                for (int i = 0; i < ArtSliderTitle.Length; i++)
+                {
+                    ArtSliderTitle[i].text = "Articulation " + (i + 1);
+                }
+                break;
+            case 1:
+                titleText.text = "Inverse Kinematics";
+                for (int i = 0; i < ArtSlider.Length; i++)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            ArtSlider[i].maxValue = 14f;
+                            ArtSliderMaxValue[i].text = "14m";
+                            ArtSlider[i].minValue = 3.01f;
+                            ArtSliderMinValue[i].text = "3m";
+                            ArtSlider[i].value = 8;
+                            break;
+                        case 1:
+                            ArtSlider[i].maxValue = 4f;
+                            ArtSliderMaxValue[i].text = "4m";
+                            ArtSlider[i].minValue = -4f;
+                            ArtSliderMinValue[i].text = "4m";
+                            ArtSlider[i].value = 0;
+                            break;
+                        case 2:
+                            ArtSlider[i].maxValue = 14f;
+                            ArtSliderMaxValue[i].text = "14m";
+                            ArtSlider[i].minValue = 0f;
+                            ArtSliderMinValue[i].text = "0m";
+                            ArtSlider[i].value = 8;
+                            break;
+                        default:
+                            ArtSlider[i].maxValue = 90f;
+                            ArtSliderMaxValue[i].text = "90°";
+                            ArtSlider[i].minValue = -90f;
+                            ArtSliderMinValue[i].text = "-90°";
+                            ArtSlider[i].value = 0;
+                            break;
+
+                    }
+                }
+                ArtSliderTitle[0].text = "  PX";
+                ArtSliderTitle[1].text = "  PY";
+                ArtSliderTitle[2].text = "  PZ";
+                ArtSliderTitle[3].text = "  RX";
+                ArtSliderTitle[4].text = "  RY";
+                ArtSliderTitle[5].text = "  RZ";
+
+                break;
+        }
+    }
 }
